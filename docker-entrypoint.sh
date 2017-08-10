604e8e06411abdf912bb45124f06ada0696ca494
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-08-10 18:03:27 -0300

    [ UPDATE ] Adding validations to install xdebug only if it isn't installed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,32 +17,35 @@ mv composer.phar /usr/local/bin/composer
 # X-Debug
 
 if ! [ -v $XDEBUG_REMOTE_ENABLE ] && [ "$XDEBUG_INSTALL" = "true" ] ; then
-    echo "[ ****************** ] Starting install of XDebug and dependencies."
-    yes | pecl install xdebug
-    echo "zend_extension="`find /usr/local/lib/php/extensions/ -iname 'xdebug.so'` > $XDEBUGINI_PATH
-    echo "xdebug.remote_enable=$XDEBUG_REMOTE_ENABLE" >> $XDEBUGINI_PATH
+	pecl shell-test xdebug && echo "Package xdebug Installed" || (
+	    echo "[ ****************** ] Starting install of XDebug and dependencies."
+	    yes | pecl install xdebug
+	    echo "zend_extension="`find /usr/local/lib/php/extensions/ -iname 'xdebug.so'` > $XDEBUGINI_PATH
+	    echo "xdebug.remote_enable=$XDEBUG_REMOTE_ENABLE" >> $XDEBUGINI_PATH
 
-    if ! [ -v $XDEBUG_REMOTE_AUTOSTART ] ; then
-        echo "xdebug.remote_autostart=$XDEBUG_REMOTE_AUTOSTART" >> $XDEBUGINI_PATH
-    fi
-    if ! [ -v $XDEBUG_REMOTE_CONNECT_BACK ] ; then
-        echo "xdebug.remote_connect_back=$XDEBUG_REMOTE_CONNECT_BACK" >> $XDEBUGINI_PATH
-    fi
-    if ! [ -v $XDEBUG_REMOTE_HANDLER ] ; then
-        echo "xdebug.remote_handler=$XDEBUG_REMOTE_HANDLER" >> $XDEBUGINI_PATH
-    fi
-    if ! [ -v $XDEBUG_PROFILER_ENABLE ] ; then
-        echo "xdebug.profiler_enable=$XDEBUG_PROFILER_ENABLE" >> $XDEBUGINI_PATH
-    fi
-    if ! [ -v $XDEBUG_PROFILER_OUTPUT_DIR ] ; then
-        echo "xdebug.profiler_output_dir=$XDEBUG_PROFILER_OUTPUT_DIR" >> $XDEBUGINI_PATH
-    fi
-    if ! [ -v $XDEBUG_REMOTE_PORT ] ; then
-        echo "xdebug.remote_port=$XDEBUG_REMOTE_PORT" >> $XDEBUGINI_PATH
-    fi
+	    if ! [ -v $XDEBUG_REMOTE_AUTOSTART ] ; then
+	        echo "xdebug.remote_autostart=$XDEBUG_REMOTE_AUTOSTART" >> $XDEBUGINI_PATH
+	    fi
+	    if ! [ -v $XDEBUG_REMOTE_CONNECT_BACK ] ; then
+	        echo "xdebug.remote_connect_back=$XDEBUG_REMOTE_CONNECT_BACK" >> $XDEBUGINI_PATH
+	    fi
+	    if ! [ -v $XDEBUG_REMOTE_HANDLER ] ; then
+	        echo "xdebug.remote_handler=$XDEBUG_REMOTE_HANDLER" >> $XDEBUGINI_PATH
+	    fi
+	    if ! [ -v $XDEBUG_PROFILER_ENABLE ] ; then
+	        echo "xdebug.profiler_enable=$XDEBUG_PROFILER_ENABLE" >> $XDEBUGINI_PATH
+	    fi
+	    if ! [ -v $XDEBUG_PROFILER_OUTPUT_DIR ] ; then
+	        echo "xdebug.profiler_output_dir=$XDEBUG_PROFILER_OUTPUT_DIR" >> $XDEBUGINI_PATH
+	    fi
+	    if ! [ -v $XDEBUG_REMOTE_PORT ] ; then
+	        echo "xdebug.remote_port=$XDEBUG_REMOTE_PORT" >> $XDEBUGINI_PATH
+	    fi
 
-    echo "xdebug.remote_host="`/sbin/ip route|awk '/default/ { print $3 }'` >> $XDEBUGINI_PATH
-    echo "[ ****************** ] Ending install of XDebug and dependencies."
+	    echo "xdebug.remote_host="`/sbin/ip route|awk '/default/ { print $3 }'` >> $XDEBUGINI_PATH
+	    echo "[ ****************** ] Ending install of XDebug and dependencies."
+
+	)
 fi
 
 echo "[ ****************** ] Ending Endpoint of Application"
